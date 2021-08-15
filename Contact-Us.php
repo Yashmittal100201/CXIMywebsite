f8e0a59ff92a418d7fdaec49d583f742ac887fc9
Yashmittal100201 <82879010+Yashmittal100201@users.noreply.github.com>
2021-08-15 15:46:52 +0530

    Update Contact-Us.php

--- Contact-Us.php
+++ Contact-Us.php
@@ -12,7 +12,7 @@ if(empty($name) || empty($email) || empty($subject) || empty($message))
     echo "Please fill all the fields."
 }
 else{
-    mail("yashmittal100201@gmial.com","CXI Message",$message,"From:$name<$email>");
+    mail("yashmittal100201@gmail.com","CXI Message",$message,"From:$name<$email>");
     echo "<script type='txt/javascript'>alert('Your message sent successfully.');
        window.history.log(-1);</script>";
 }
