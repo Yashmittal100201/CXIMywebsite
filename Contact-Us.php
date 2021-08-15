efafcb277596aa2cd67264236872c32f3b10f6bd
Yashmittal100201 <82879010+Yashmittal100201@users.noreply.github.com>
2021-08-15 15:39:04 +0530

    Create Contact-Us.php

---
+++ Contact-Us.php
@@ -0,0 +1,19 @@
+<?php
+//  Declaration of Variable in php
+
+$name = $_REQUEST['NameOfUser']
+$email = $_REQUEST['EmailAddressOfUser']
+$subject = $_REQUEST['Subject']
+$message = $_REQUEST['UsersMessage']
+
+// Check input fields
+if(empty($name) || empty($email) || empty($subject) || empty($message))
+{
+    echo "Please fill all the fields."
+}
+else{
+    mail("yashmittal100201@gmial.com","CXI Message",$message,"From:$name<$email>");
+    echo "<script type='txt/javascript'>alert('Your message sent successfully.');
+       window.history.log(-1);</script>";
+}
+?>
